ecb3060f2d8d18118d3e784afc092e71b6e73246
nemo <logentix@gmail.com>
2019-12-17 23:20:06 -0500

    Cleaning up code

---
+++ README.md
@@ -0,0 +1,34 @@
+# Coinbase Pro Correlation Analysis
+
+The purpose of this project is to run a correlation analysis on the price and volume movements of all trading pairs on Coinbase Pro via the API at: https://api.pro.coinbase.com
+
+## Why?
+
+A correlation analysis can be used as a starting point for creating a diversified trading portfolio, or creating a strategy like mean reversal. This is meant as a starting point for others to build upon. 
+
+## Requirements
+
+Python 3.6.x or greater.
+
+## Usage
+
+1. Create a virtual environment `./make-env.sh`
+2. Activate that environment `source ./activate-env.sh`
+3. Install required packages `pip install -r requirements.txt`
+4. Run Jupyter Notebook `jupyter notebook`
+
+
+## Disclaimer
+I am not responsible in any way for profits or losses made using this code. Use at your own risk.
+
+## License
+The MIT License (MIT)
+
+Copyright (c) Randy Lopez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- main.cpp
+++ main.cpp
@@ -15,28 +15,34 @@ using namespace cv::xfeatures2d;
 
 int main()
 {
+  // camera
   VideoCapture cap(0);
-  VideoCapture vid("videoplayback.mp4"); // load some fire
+  
+  // video to render over QR code
+  VideoCapture vid("videoplayback.mp4"); 
+
   Mat img_object, img_scene, descriptors_object, descriptors_scene, img_matches;;
   std::vector<KeyPoint> keypoints_object, keypoints_scene;
   FlannBasedMatcher matcher;
 
-  //load template image
+  // load QR code which we will track
   img_object = imread("QR.png", IMREAD_GRAYSCALE );
   resize(img_object, img_object, Size(400,400));
-  //img_scene = imread( "test.jpg", IMREAD_GRAYSCALE );
+
   if( !img_object.data)
   { std::cout<< " --(!) Error reading image " << std::endl; return -1; }
 
-  //-- Step 1: Detect the keypoints and extract descriptors using SURF
+  // detect the keypoints and extract descriptors using SURF
   Ptr<SURF> detector = SURF::create(MIN_HESSIAN);
+
   // detect the SURF keypoints in the object image
   detector->detectAndCompute( img_object, Mat(), keypoints_object, descriptors_object );
 
-  Mat H_latest = Mat::eye(3, 3, CV_32F); // H is used to transform the video mat over the scene mat. H_old is used to store the last reasonable transform
+  // H is used to transform the video mat over the scene mat. H_old is used to store the last reasonable transform
+  Mat H_latest = Mat::eye(3, 3, CV_32F); 
   Mat scene_mask  = Mat::zeros(img_scene.rows, img_scene.cols, CV_8UC1);
 
-  //main loop
+  // main loop
   while(1){
     bool good_detection = false;
 
@@ -48,20 +54,20 @@ int main()
     scene_mask = Mat::zeros(img_scene.rows, img_scene.cols, CV_8UC1);
 
     detector->detectAndCompute( img_scene, Mat(), keypoints_scene, descriptors_scene );
-    //-- Step 2: Matching descriptor vectors using FLANN matcher
+    // match descriptor vectors using FLANN matcher
 
     std::vector< DMatch > matches;
     matcher.match( descriptors_object, descriptors_scene, matches );
     double max_dist = 0; double min_dist = 100;
-    //-- Quick calculation of max and min distances between keypoints
+
+    //  Quick calculation of max and min distances between keypoints
     for( int i = 0; i < descriptors_object.rows; i++ )
     { double dist = matches[i].distance;
       if( dist < min_dist ) min_dist = dist;
       if( dist > max_dist ) max_dist = dist;
     }
-    //printf("-- Max dist : %f \n", max_dist );
-    //printf("-- Min dist : %f \n", min_dist );
-    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+
+    // draw only "good" matches (i.e. whose distance is less than 3*min_dist )
     std::vector< DMatch > good_matches;
     for( int i = 0; i < descriptors_object.rows; i++ )
     { if( matches[i].distance < 3*min_dist )
@@ -71,25 +77,28 @@ int main()
     drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
                  good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                  std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-    if(img_matches.data) //check if there are any matches, if so then continue
+
+    // check if there are any matches, if so then continue
+    if(img_matches.data) 
     {
-      //-- Localize the object
+      // localize the object
       std::vector<Point2f> obj;
       std::vector<Point2f> scene;
       for( size_t i = 0; i < good_matches.size(); i++ )
       {
-        //-- Get the keypoints from the good matches
+        // Get the keypoints from the good matches
         obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
         scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
       }
-      //this returns a 3x3 transformation matrix. If none could be found continue
-      //to the next iteration.
+
+      // this returns a 3x3 transformation matrix. If none could be found continue
+      // to the next iteration.
       Mat H = findHomography( obj, scene, RANSAC );
       if (!H.data){
         continue;
       }
 
-      //-- Get the corners from the image_1 ( the object to be "detected" )
+      // get the corners from the image_1 ( the object to be "detected" )
       std::vector<Point2f> obj_corners(4);
       obj_corners[0] = Point(0, 0);
       obj_corners[1] = Point( img_object.cols, 0 );
@@ -98,14 +107,14 @@ int main()
       std::vector<Point2f> scene_corners(4);
       perspectiveTransform( obj_corners, scene_corners, H);
 
-      // Check the determinent of the transormation matrix to make sure it's not crazy
+      // check the determinent of the transormation matrix to make sure it's not crazy
       float hDet = abs(determinant(H));
       if (hDet < 100 && hDet > 0.05){ // Good detection, reasonable transform
         H_latest = H;
         good_detection = true;
       }
 
-      //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+      // draw lines between the corners (the mapped object in the scene - image_2 )
 
       std::vector<Point2f> match_corners(4);
       match_corners[0] = scene_corners[0] + Point2f( img_object.cols, 0);
@@ -113,18 +122,16 @@ int main()
       match_corners[2] = scene_corners[1] + Point2f( img_object.cols, 0);
       match_corners[3] = scene_corners[2] + Point2f( img_object.cols, 0);
 
-      //line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+      // line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
       line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
       line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
       line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-      //-- Show detected matches
-      //imshow( "SURF", img_matches );
 
       Mat img_video;
       vid >> img_video;
 
 
-      // Create the mask for the video in the scene veiw
+      // create the mask for the video in the scene view
       std::vector<Point2f> vid_corners(4);
       vid_corners[0] = Point( 0, 0 );
       vid_corners[1] = Point( img_video.cols, 0 );
@@ -138,15 +145,15 @@ int main()
       fillConvexPoly(scene_mask, nonfloat_corners, 4, cv::Scalar(255));
       warpPerspective( scene_mask, scene_mask, H_latest, Size(img_scene.cols,img_scene.rows));
 
-      //Apply transformation to video image and copy with mask
+      // apply transformation to video image and copy with mask
       warpPerspective( img_video, img_video, H_latest, Size(img_scene.cols,img_scene.rows));
 
       if(   good_detection ){
         img_video.copyTo(img_scene, scene_mask);
       }
-
+      
+      // render scene
       imshow( "Test", img_scene);
-
       waitKey(1);
     }
   }
